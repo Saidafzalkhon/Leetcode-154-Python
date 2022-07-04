b91f1325671603ddaaa9abf47b0101ad09c885b1
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2022-07-04 19:03:48 +0500

    Add commit

--- Leetcode-154.py
+++ Leetcode-154.py
@@ -1,3 +1,16 @@
-nums = list(map(int,input().split()))
-nums.sort()
-print(nums[0])
+n = int(input())
+son = 0
+raqam = 1
+while son != n:
+    son+=1
+    raqam*= son
+if n > 0:
+    son = 0
+    for i in str(raqam)[::-1]:
+        if i == '0':
+            son+=1
+        else:
+            break
+    print(son)
+else:
+    print(0)
